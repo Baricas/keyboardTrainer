edb953ede25eb55c16b98ff8b1f0351b2bd3e98e
Neest <denyska7@yandex.ru>
2017-01-22 00:38:04 +0200

    Добавил для #2: Структура проекта

--- src/js/generator.js
+++ src/js/generator.js
@@ -2,12 +2,9 @@ const rand = (l, h) => Math.floor((h-l+1)*Math.random())+Math.floor(l);
 
 window.getCombination = (letters, min, max) => {
   let output = [];
-  
-  let length = rand(min, max);
 
-  for(let i = 0; i < length; i++) {
+  for(let i = 0; i < rand(min, max); i++)
     output.push(letters[rand(0, letters.length - 1)]);
-  }
 
   return output.join('');
 };
